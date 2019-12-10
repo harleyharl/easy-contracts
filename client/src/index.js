76ae86f83436cca3fcaf29d57d4034825b13cc6e
harleyharl <harleymteden@gmail.com>
2019-12-10 15:53:54 -0800

    created store for redux, passed in the reducers and set up thunk middleware for async requests

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Root from './Root'
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, compose(applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
+
+ReactDOM.render(<Root store={store} />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
