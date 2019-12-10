559ccb5e7a20d85f265e50b39d7f79dbb83205ec
harleyharl <harleymteden@gmail.com>
2019-12-10 15:51:16 -0800

    created a NotFound catchall component

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = (props) => {
+  return (
+    <div>
+      <div className="notFoundContainer">
+        <p>Sorry, theres nothing here!</p>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
