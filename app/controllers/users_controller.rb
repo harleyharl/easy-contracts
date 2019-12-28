1d50716aa2f3dbb2f65506a884bf1bc58c16c7d2
harleyharl <harleymteden@gmail.com>
2019-12-27 22:19:39 -0800

    sketched out user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.valid?
+      @token = encode_token({ user_id: @user.id })
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+  end
+
+  def update
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :bio, :avatar, :password)
+  end
 end
