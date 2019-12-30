be677cf031d132747e595f5e6aa9e5e7936351df
harleyharl <harleymteden@gmail.com>
2019-12-30 11:39:33 -0800

    added routes for profile, signup and login

--- client/src/Root.js
+++ client/src/Root.js
@@ -5,15 +5,19 @@ import { Provider } from 'react-redux'
 import App from './App'
 import NotFound from './components/NotFound';
 import Login from './containers/Login'
+import Signup from './containers/Signup'
+import Profile from './containers/Profile'
 
 const history = createBrowserHistory();
 
 const Root = ({ store }) =>
 <Provider store={store}>
   <Router history={history}>
-    <Switch>
+    <Switch history={history}>
       <Route exact path="/" component={App} />
-      <Route exact path="/login" component={Login} />
+      <Route exact path="/login" history={history} component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/profile" component={Profile} history={history}/>
       <Route component={NotFound} />
     </Switch>
   </Router>
