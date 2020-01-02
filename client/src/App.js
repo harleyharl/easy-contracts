4e996060db20b2651c392bf941a0709bc4df8d53
harleyharl <harleymteden@gmail.com>
2020-01-01 18:11:21 -0800

    added functional profile link and logout buttons and imported corresponding actions. connected to redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {getProfileFetch, logoutUser} from './actions/actions.js';
+import Signup from './containers/Signup';
+import Login from './containers/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        easy contracts
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  componentDidMount = (history) => {
+
+    this.props.getProfileFetch(history)
+  }
+
+  handleClick = event => {
+    event.preventDefault()
+    // Remove the token from localStorage
+    localStorage.removeItem("token")
+    // Remove the user object from the Redux store
+    this.props.logoutUser()
+  }
+
+  render() {
+    debugger
+    return (
+      <div>
+      <Link to='/profile'>
+       go profile
+      </Link>
+        {this.props.currentUser.currentUser.username
+          ? <button onClick={this.handleClick}>Log Out</button>
+          : null
+        }
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  currentUser: state.currentUser
+})
+
+const mapDispatchToProps = dispatch => ({
+  getProfileFetch: () => dispatch(getProfileFetch()),
+  logoutUser: () => dispatch(logoutUser())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
