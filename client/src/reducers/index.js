c91191e23c3fda7a981e39e8baafaf22dae11b10
harleyharl <harleymteden@gmail.com>
2020-01-01 18:14:15 -0800

    added usersReducer to combine reducers function

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import contractsReducer from './contracts_reducer'
+import usersReducer from './users_reducer'
 
 const rootReducer = combineReducers({
   contracts: contractsReducer,
+  currentUser: usersReducer
 });
 
 export default rootReducer;
