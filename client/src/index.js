8188ca99d68c01ebb6718abcc9c8b98d750de744
harleyharl <harleymteden@gmail.com>
2020-01-03 09:15:32 -0800

    enclosed App as a Route component instead of Root.

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './Root'
+import App from './App'
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 import * as serviceWorker from './serviceWorker';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+// import { ConnectedRouter, routerMiddleware } from 'connected-react-router';
+import { createBrowserHistory } from 'history'
+
+export const history = createBrowserHistory()
+
+
+
 const store = createStore(rootReducer, compose(applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ))
 
-ReactDOM.render(<Root store={store} />, document.getElementById('root'))
+ReactDOM.render(
+    <Provider store={store}>
+      <Router history={history}>
+        <Route component={App} />
+      </Router>
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
