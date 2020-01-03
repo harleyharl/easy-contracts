149bc8d332d32fa0911584865006ed7d6a4c17e4
harleyharl <harleymteden@gmail.com>
2020-01-03 09:15:51 -0800

    removed contracts reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
-import contractsReducer from './contracts_reducer'
 import usersReducer from './users_reducer'
 
 const rootReducer = combineReducers({
-  contracts: contractsReducer,
   currentUser: usersReducer
 });
 
