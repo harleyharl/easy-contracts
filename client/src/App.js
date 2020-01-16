c8794d98d5972774060aec1265660f4b893a78ab
harleyharl <harleymteden@gmail.com>
2020-01-16 10:57:19 -0800

    added private route but it still doesn't work!

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link, Redirect } from 'react-router-dom';
 import {connect} from 'react-redux';
 import {getProfileFetch, logoutUser} from './actions/actions.js';
 import Signup from './containers/Signup';
@@ -8,12 +8,22 @@ import Profile from './containers/Profile'
 import Home from './components/Home'
 import NotFound from './components/NotFound'
 
+const PrivateRoute = ({ component: Component, ...props }) => {
+  debugger
+  return (
+    <Route {...props} render={innerProps => innerProps.auth ? <Component {...innerProps} /> : <Redirect to="/login" /> }/>
+  );
+};
 
 class App extends Component {
 
   componentDidMount = (history) => {
     this.props.getProfileFetch(history)
   }
+  //
+  // auth = {
+  //   isAuthed: this.props.currentUser.authed
+  // }
 
   handleClick = event => {
     event.preventDefault()
@@ -24,11 +34,12 @@ class App extends Component {
   }
 
   render() {
+    debugger
     return (
       <div>
         <Switch>
           <Route path="/login" component={Login} />
-          <Route path="/profile" component={Profile} />
+          <PrivateRoute exact path="/profile" component={Profile} auth={this.props.currentUser.authed} />
           <Route path="/signup" component={Signup} />
           <Route exact path="/" component={Home} />
           <Route path="/" component={NotFound} />
