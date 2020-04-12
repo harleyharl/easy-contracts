06f43d2a46d469abea26a7f72928004027e5e38d
harleyharl <harleymteden@gmail.com>
2020-04-11 17:53:06 -0700

    renders form. need to fix what happens when someone is logged in already (they need to go to dashboard)

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {userLoginFetch} from '../actions/actions.js';
+import { Redirect } from 'react-router-dom'
 
 class Login extends Component {
   state = {
@@ -20,30 +21,35 @@ class Login extends Component {
   }
 
   render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <h1>Login</h1>
+    debugger
+    if (localStorage.token !== null){
+      return(
+        <form onSubmit={this.handleSubmit}>
+          <h1>Login</h1>
 
-        <label>Username</label>
-        <input
-          name='username'
-          placeholder='Username'
-          value={this.state.username}
-          onChange={this.handleChange}
-          /><br/>
+          <label>Username</label>
+          <input
+            name='username'
+            placeholder='Username'
+            value={this.state.username}
+            onChange={this.handleChange}
+            /><br/>
 
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          placeholder='Password'
-          value={this.state.password}
-          onChange={this.handleChange}
-          /><br/>
+          <label>Password</label>
+          <input
+            type='password'
+            name='password'
+            placeholder='Password'
+            value={this.state.password}
+            onChange={this.handleChange}
+            /><br/>
 
-        <input type='submit'/>
-      </form>
-    )
+          <input type='submit'/>
+        </form>
+      )
+    } else {
+      return(<Redirect to="/login" />)
+    }
   }
 }
 
