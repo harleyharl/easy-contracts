0ead8a841b1681ec094eeb990114eb750e10ede8
harleyharl <harleymteden@gmail.com>
2020-04-12 21:22:29 -0700

    created contracts_controller

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,2 +1,25 @@
 class ContractsController < ApplicationController
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    binding.pry
+    @contract = Contract.create(contract_params)
+    if @contract.valid?
+      render json: { contract: ContractSerializer.new(@contract)}, status: :created
+    else
+      render json: { error: 'failed to create contract' }, status: :not_acceptable
+    end
+  end
+
+  def update
+  end
+
+  def contract_params
+    params.require(:contract).permit(:project_name, :contract_name)
+  end
 end
