70188a8009852d54f59c5610a095247c14f0bd19
harleyharl <harleymteden@gmail.com>
2020-04-12 21:22:50 -0700

    just reformatted ugly code

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,9 +4,10 @@ class UserSerializer < ActiveModel::Serializer
   # has_many :contracts
 
   def user
-    {username: self.object.username,
-     avatar: self.object.avatar,
-     bio: self.object.bio
+    {
+      username: self.object.username,
+      avatar: self.object.avatar,
+      bio: self.object.bio
     }
   end
 end
